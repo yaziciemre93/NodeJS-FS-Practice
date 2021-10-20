8801cda2eb9270eeec04cc10a6805f952b0be972
EMRE YAZICI <emre@EMREs-MacBook-Pro.local>
2021-10-20 18:07:57 +0300

    first init

---
+++ index.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+
+/* fs.writeFile(
+  "employees.json",
+  `{ "name": "Employee1", "salary": 2000 }`,
+  "utf-8",
+  (err) => {
+    if (err) console.log(err);
+    console.log("EMPLOYEE WAS CREATED");
+  }
+); */
+
+/* fs.readFile("employees.json", "utf-8", (err, data) => {
+  if (err) console.log(err);
+  console.log(data);
+  console.log("READING FILE");
+});
+ */
+
+/* fs.writeFile(
+  "employees.json",
+  `{ "name": "Employee1", "salary": 3000 }`,
+  (err) => {
+    if (err) console.log(err);
+    console.log("UPDATING FILE");
+  }
+); */
+
+/* fs.unlink("employees.json", (err) => {
+  if (err) console.log(err);
+  console.log("FILE HAS BEEN DELETED");
+}); */
